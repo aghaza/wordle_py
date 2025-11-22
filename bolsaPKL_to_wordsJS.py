9aeb379d5ff2401a2879380b3399ff1058d0f96f
aghaza <97561776+aghaza@users.noreply.github.com>
2025-11-22 15:25:57 -0300

    Add files via upload

--- bolsaPKL_to_wordsJS.py
+++ bolsaPKL_to_wordsJS.py
@@ -1,7 +1,10 @@
-    *WORDLE* python.    
-    
-    Creado por George Aghazarian - _aghazarian@pm.me_
-    Última versión: 21 de noviembre de 2025 - 18:30
+''' Convierte el archivo binario "bolsa.pkl" a "words.js" 
+    generando una constande en JS de las palabras para uso en la versión web.
+
+    Creado por George Aghazarian - aghazarian@pm.me
+    Última versión: 21 de noviembre de 2025 - 15:20
+
+    Este programa es un accesorio complementario.
 
     Archivos imprescindibles con al menos un jugador registrado (no se permitirán nuevos registros):
         main.py
@@ -10,18 +13,42 @@
     Archivos imprescindibles en la carpeta si no hay jugadores registrados:
         main.py
         bolsa.pkl
-    
+        
     Archivos totales:
         main.py         		- programa principal
         generador.py    		- genera la base de palabras bolsa.pkl a partir de un archivo a elegir que contenga las palabras
         agregador.py    		- gestor del archivo bolsa.pkl que permite agregar, eliminar, contabilizar y listar las palabras
         conversor.py    		- convierte bolsa.pkl a bolsa_set.py cuyo contenido es bolsa = {aquí dentro un set con las palabras}
-        list2bin.py			- proceso iverso a conversor.py
+        list2bin.py				- proceso iverso a conversor.py
         wordsJS_to_pyPKL.py		- igual a list2bin.py pero a partir de words.js
-        bolsaPKL_to_wordsJS.py		- proceso inverso a wordsJS_to_pyPKL.py
+        bolsaPKL_to_wordsJS.py	- proceso inverso a wordsJS_to_pyPKL.py
         bolsa.pkl       		- archivo que contiene la fuente de palabras original para crear un jugador
         frecuentes.txt  		- base de las palabras más frecuentes del castellano
         es.dic          		- corrector ortográfico de LibreOfiice para usar de base de miles de plabras
         icono.png       		- icono de WORDLE
 
     Requiere python3
+
+'''
+
+from os import system, path
+if system("clear") != 0: system("cls")  # Limpiar pantalla
+import subprocess
+import pickle
+import sys
+
+if path.exists('bolsa.pkl'):
+    # Recuperación del archivo bolsa de palabras
+    with open('bolsa.pkl', 'rb') as archivo:
+        bolsa = pickle.load(archivo)
+else:
+    print('No está presente el archivo <bolsa.pkl>')
+    sys.exit()
+
+bolsa = str(bolsa)
+bolsa = bolsa.strip("{}")
+bolsa = 'const WORDS = [' + str(bolsa) + "];"
+
+destino = open('words.js', 'w')
+destino.write(bolsa)
+destino.close()
--- main.py
+++ main.py
@@ -20,16 +20,17 @@
         bolsa.pkl
       
     Archivos totales:
-        main.py         	- programa principal
-        generador.py    	- genera la base de palabras bolsa.pkl a partir de un archivo a elegir que contenga las palabras
-        agregador.py    	- gestor del archivo bolsa.pkl que permite agregar, eliminar, contabilizar y listar las palabras
-        conversor.py    	- convierte bolsa.pkl a bolsa_set.py cuyo contenido es bolsa = {aquí dentro un set con las palabras}
-        list2bin.py			- proceso iverso a conversor.py
-        wordsJS_to_pyPKL.py	- igual a list2bin.py pero a partir de words.js
-        bolsa.pkl       	- archivo que contiene la fuente de palabras original para crear un jugador
-        frecuentes.txt  	- base de las palabras más frecuentes del castellano
-        es.dic          	- corrector ortográfico de LibreOfiice para usar de base de miles de plabras
-        icono.png       	- icono de WORDLE
+        main.py         		- programa principal
+        generador.py    		- genera la base de palabras bolsa.pkl a partir de un archivo a elegir que contenga las palabras
+        agregador.py    		- gestor del archivo bolsa.pkl que permite agregar, eliminar, contabilizar y listar las palabras
+        conversor.py    		- convierte bolsa.pkl a bolsa_set.py cuyo contenido es bolsa = {aquí dentro un set con las palabras}
+        list2bin.py				- proceso iverso a conversor.py
+        wordsJS_to_pyPKL.py		- igual a list2bin.py pero a partir de words.js
+        bolsaPKL_to_wordsJS.py	- proceso inverso a wordsJS_to_pyPKL.py
+        bolsa.pkl       		- archivo que contiene la fuente de palabras original para crear un jugador
+        frecuentes.txt  		- base de las palabras más frecuentes del castellano
+        es.dic          		- corrector ortográfico de LibreOfiice para usar de base de miles de plabras
+        icono.png       		- icono de WORDLE
 
     Requiere python3
 '''
